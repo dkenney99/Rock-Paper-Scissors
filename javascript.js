2147b609671f8e2d17501eef48efff1ee532716d
Daniel Kenney <danielkenney99@yahoo.com>
2022-06-12 20:46:31 -0700

    added the score. code works perfect! W00t!

--- javascript.js
+++ javascript.js
@@ -18,16 +18,22 @@ function playRound(playerSelection, computerSelection) {
   ) {
     return "It is a tie!";
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    ++winCount;
     return "You Win! Rock beats Scissors";
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    ++winCount;
     return "You Win! Paper beats Rock";
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    ++winCount;
     return "You Win! Scissors beats Paper";
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    ++lossCount;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    ++lossCount;
     return "You Lose! Rock beats Scissors";
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    ++lossCount;
     return "You Lose! Scissors beats Paper";
   }
 }
@@ -35,11 +41,16 @@ function playRound(playerSelection, computerSelection) {
 function game() {
   playerSelection = convertInput(prompt("Rock, paper, or scissors?"));
   console.log(playRound(playerSelection, computerSelection));
+  console.log(`Game score: ${winCount} - ${lossCount}`);
 }
 
+let winCount = 0;
+let lossCount = 0;
 let playerSelection = "";
 const computerSelection = computerPlay();
 
 for (let i = 0; i < 5; i++) {
   game();
 }
+
+console.log;
