4c4913faede2e3614bfe9f97817e95fb1693b9c1
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-23 16:50:20 -0700

    logic for the rounds completed

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ function getComputerChoice() {
     return allChoices[choice];
 }
 
-console.log(getComputerChoice())
-
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Paper"
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You win! Scissors beats Paper!"
+    }
+}
