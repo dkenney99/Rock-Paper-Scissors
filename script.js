0d94bcc717f3a5f68261c7f4de528437bc83d344
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-24 12:36:39 -0700

    game logic runs 5 times but broken

--- script.js
+++ script.js
@@ -8,17 +8,26 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return "It's a tie!"
-    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    } else if (playerSelection == "rock" && computerSelection == "Paper") {
         return "You Lose! Paper beats Rock"
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
         return "You Win! Rock beats Paper"
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
         return "You Win! Paper beats Rock"
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
         return "You Lose! Scissors beats Paper"
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
         return "You Lose! Rock beats Scissors"
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
         return "You win! Scissors beats Paper!"
+    } else {
+        return "Invalid input. Please input rock, paper, or scissors"
     }
+}
+
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Rock, paper, or scissors?");
+    let input = playerSelection.toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(playRound(input, computerSelection));
 }
