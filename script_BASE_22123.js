60c9bee00b8f40b93bd38473a31a50c2571bc1c1
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-26 22:17:28 -0700

    done with backups

--- script_BASE_22123.js
+++
@@ -1,9 +0,0 @@
-const allChoices = ["Rock", "Paper", "Scissors"]
-
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3)
-    return allChoices[choice];
-}
-
-console.log(getComputerChoice())
-
